a8ea2b4bd51b407d3a41ed4febd4d2fd8d24f7e1
Margot Konig <margot.konig@blackrock.com>
2019-06-12 22:58:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,6 +43,19 @@ class Student
       self.new_from_db(row)
     end
   end
+  
+   def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+ 
+    DB[:conn].execute(sql, num).map do |row|
+      self.new_from_db(row)
+    end
+  end
 
  def self.find_by_name(name)
     sql = <<-SQL
