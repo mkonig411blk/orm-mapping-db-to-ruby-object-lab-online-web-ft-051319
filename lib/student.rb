75c93895b620342f409681892bf4b3d2b32bd490
Margot Konig <margot.konig@blackrock.com>
2019-06-12 22:59:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,11 +32,11 @@ class Student
     end
   end
   
-  def self.all_students_in_grade_12
+  def self.all_students_below_12th_grade
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 12
+      WHERE grade < 12
     SQL
  
     DB[:conn].execute(sql).map do |row|
