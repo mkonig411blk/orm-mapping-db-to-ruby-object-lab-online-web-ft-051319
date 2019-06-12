d68e47c80df4f64e29ecbaa20b43430fdea12b39
Margot Konig <margot.konig@blackrock.com>
2019-06-12 22:57:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,6 +31,18 @@ class Student
       self.new_from_db(row)
     end
   end
+  
+  def self.all_students_in_grade_12
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 12
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
 
  def self.find_by_name(name)
     sql = <<-SQL
